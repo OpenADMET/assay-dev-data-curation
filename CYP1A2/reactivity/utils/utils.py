530048c9ce32ed50f60e460e4049e31e1ccf34f5
Devany West <dwest@rockefeller.edu>
2025-01-15 16:28:05 -0500

    Worked on reactivity, combined data, only had 70 compounds

---
+++ CYP1A2/reactivity/utils/utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from rdkit import Chem
+import seaborn as sns
+from tqdm import tqdm
+import numpy as np
+from rdkit.Chem.MolStandardize import rdMolStandardize
+
+
+# from Pat Walters method
+def standardize_smiles(smiles):
+    try:
+        # follows the steps in
+        # https://github.com/greglandrum/RSC_OpenScience_Standardization_202104/blob/main/MolStandardize%20pieces.ipynb
+        # as described **excellently** (by Greg) in
+        # https://www.youtube.com/watch?v=eWTApNX8dJQ
+        mol = Chem.MolFromSmiles(smiles)
+    
+        # removeHs, disconnect metal atoms, normalize the molecule, reionize the molecule
+        clean_mol = rdMolStandardize.Cleanup(mol)
+    
+        # if many fragments, get the "parent" (the actual mol we are interested in) 
+        parent_clean_mol = rdMolStandardize.FragmentParent(clean_mol)
+    
+        # try to neutralize molecule
+        uncharger = rdMolStandardize.Uncharger()  # annoying, but necessary as no convenience method exists
+        uncharged_parent_clean_mol = uncharger.uncharge(parent_clean_mol)
+    
+        # note that no attempt is made at reionization at this step
+        # nor at ionization at some pH (rdkit has no pKa caculator)
+        # the main aim to represent all molecules from different sources
+        # in a (single) standard way, for use in ML, catalogue, etc.
+    
+        te = rdMolStandardize.TautomerEnumerator()  # idem
+        taut_uncharged_parent_clean_mol = te.Canonicalize(uncharged_parent_clean_mol)
+    
+        return Chem.MolToSmiles(taut_uncharged_parent_clean_mol)
+    except:
+        return pd.NA
+    
+def smi_to_inchikey(smiles):
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+        return Chem.MolToInchiKey(mol)
+    except:
+        return pd.NA
